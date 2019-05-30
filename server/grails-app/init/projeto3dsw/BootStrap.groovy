97833e122410d668cd1d49268578bbfce53472bf
Matheus Bortoleto <matheus.silva.ufscar@gmail.com>
2019-05-30 13:37:35 -0300

    Adiciona role de usuario

--- server/grails-app/init/projeto3dsw/BootStrap.groovy
+++ server/grails-app/init/projeto3dsw/BootStrap.groovy
@@ -3,14 +3,11 @@ package projeto3dsw
 class BootStrap {
 
     def init = { servletContext ->
+        def userRole = new Role(authority: 'ROLE_USER').save()
         def adminRole = new Role(authority: 'ROLE_ADMIN').save()
         def adminUser = new User(username: 'admin@email.com', password: 'admin').save()
 
         UserRole.create(adminUser, adminRole, true)
-
-        assert Role.count() == 1
-        assert User.count() == 1
-        assert UserRole.count() == 1
     }
     def destroy = {
     }
