ea0b123b2f5805485f0761e8c2b3c9f4c3885e4a
Matheus Bortoleto <matheus.silva.ufscar@gmail.com>
2019-05-30 14:26:44 -0300

    Adiciona footer basico

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatIconModule, MatToolbarModule } from '@angular/material';
 import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
