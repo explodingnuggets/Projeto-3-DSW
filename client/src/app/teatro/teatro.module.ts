a6973ab967bfb830841e2d3f4ca8260a5de4fa03
Matheus Bortoleto <matheus.silva.ufscar@gmail.com>
2019-05-31 17:21:58 -0300

    Adiciona tela de listagem de teatros

--- client/src/app/teatro/teatro.module.ts
+++ client/src/app/teatro/teatro.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListComponent } from './list/list.component';
+import { TeatroRoutingModule } from './teatro-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [ListComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    TeatroRoutingModule
   ]
 })
 export class TeatroModule { }
