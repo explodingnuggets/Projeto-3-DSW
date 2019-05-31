2c2d04ae10151f344f7d8c7c44fd161094893e0f
Matheus Bortoleto <matheus.silva.ufscar@gmail.com>
2019-05-31 00:53:38 -0300

    Adiciona tela de criaçao de sites

--- client/src/app/site/site-routing.module.ts
+++ client/src/app/site/site-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListComponent } from './list/list.component';
 import { DetailComponent } from './detail/detail.component';
+import { CreateComponent } from './create/create.component';
 
 const routes: Routes = [
     {path: 'site', component: ListComponent},
+    {path: 'site/create', component: CreateComponent},
     {path: 'site/:id', component: DetailComponent}
 ];
 
--- client/src/app/site/site.module.ts
+++ client/src/app/site/site.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListComponent } from './list/list.component';
 import { SiteRoutingModule } from './site-routing.module';
-import { MatTableModule, MatSnackBarModule } from '@angular/material';
+import { MatTableModule, MatSnackBarModule, MatButtonModule } from '@angular/material';
 import { HttpClientModule } from '@angular/common/http';
 import { DetailComponent } from './detail/detail.component';
+import { CreateComponent } from './create/create.component';
 
 @NgModule({
-  declarations: [ListComponent, DetailComponent],
+  declarations: [ListComponent, DetailComponent, CreateComponent],
   imports: [
     CommonModule,
     HttpClientModule,
     SiteRoutingModule,
+    MatButtonModule,
     MatSnackBarModule,
     MatTableModule
   ]
