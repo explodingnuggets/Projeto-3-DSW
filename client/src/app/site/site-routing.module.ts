1256b110454ccf5a2d2fa5c1ee23553d4e330b8a
Matheus Bortoleto <matheus.silva.ufscar@gmail.com>
2019-05-31 02:25:30 -0300

    Remove tela e rota de criaçao

--- client/src/app/site/site-routing.module.ts
+++ client/src/app/site/site-routing.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListComponent } from './list/list.component';
 import { DetailComponent } from './detail/detail.component';
-import { CreateComponent } from './create/create.component';
 
 const routes: Routes = [
     {path: 'site', component: ListComponent},
-    {path: 'site/create', component: CreateComponent},
     {path: 'site/:id', component: DetailComponent}
 ];
 
